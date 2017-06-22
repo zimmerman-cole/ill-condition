c35f7070e6ae78ccd01f0dcf2148caeaa1a5e849
Cole Zimmerman <czimmer112@gmail.com>
2017-06-22 14:33:35 -0500

    Update scratch() GD visual

--- code/util.py
+++ code/util.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+import optimize, visual
 
 def mat_from_cond(cond_num, m=50, n=50, min_sing=None):
     """
@@ -105,18 +106,24 @@ def ghetto_command_line():
             traceback.print_exc()
 
 def scratch():
-    A = psd_with_cond(cond_num=55,n=2)
+    """
+    Visual GD.
+    """
+    A = psd_with_cond(cond_num=1000,n=2)
     x_true = np.random.randn(2)
     b = np.dot(A,x_true)
     evals,evecs = la.eig(A)
-    print('eigenvalues are: %s' % evals)
+    #print('eigenvalues are: %s' % evals)
 
-    x_opt = gradient_descent(A,b)
+    x_opt = optimize.gradient_descent(A,b, x=np.array([2.0,2.0]))
+    path = visual.gd_path(A, b, x=np.array([2.0,2.0]))
+    #print(path[0])
+    span = np.sqrt((path[0][0] - x_opt[0])**2 + (path[0][1] - x_opt[1])**2)
     # print(la.norm(x_true-x_opt))
 
     num = 100
     # x1 = x2 = np.linspace(-evals[1], evals[0], num)
-    x1 = x2 = np.linspace(-3, 3, num)
+    x1 = x2 = np.linspace(-span, span, num)
     x1v, x2v = np.meshgrid(x1, x2, indexing='ij', sparse=False)
     hv = np.zeros([num,num])
 
@@ -126,14 +133,17 @@ def scratch():
             xx = np.array([x1v[i,j],x2v[i,j]])
             hv[i,j] = np.dot(xx.T,np.dot(A,xx))-np.dot(b.T,xx)
 
-    print(hv)
+    #print(hv)
     fig = plt.figure(1)
     ax = fig.gca()
     # ax.contour(x1v, x2v, hv,50)
-    ll = np.linspace(0.001,4,20)
+    ll = np.linspace(0.0000000001,4,20)
     ll = 10**ll
     cs = ax.contour(x1v, x2v, hv,levels=ll)
     plt.clabel(cs)
     plt.axis('equal')
+    plt.plot([p[0] for p in path], [p[1] for p in path], marker='o')
+    plt.plot(x_true[0], x_true[1], marker='D', markersize=25) # TRUE POINT
+    plt.plot(path[0][0], path[0][1], marker='x', markersize=25) # STARTING POINT
+    print('num iter: %d' % len(path))
     plt.show()
-
