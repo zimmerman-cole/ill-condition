4193dc3dc2c528d29807273fbbc94738d1a16b1d
jakeroth <jmroth@uchicago.edu>
2017-07-19 12:17:16 -0500

    adding Discrete Radon Transform gen_X method

--- code/tomo2D/drt.py
+++ code/tomo2D/drt.py
@@ -1,66 +1,71 @@
 import numpy as np
+import scipy.sparse as sparse
 import pprint
 
-K = 3
-M = 10
-N = 5
 
-# name  |  type            |  description
+# name        |  type                  |  description
 # ------------------------------------------------------------------------------
-# X     |  [K, M*N] array  |  X-ray transform matrix containing ray lengths
-# X_k   |  [1, M*N] array  |  single row of X-ray transform matrix
+# X           |  [m, n_1 * n_2] array  |  X-ray transform matrix of ray line ints
+# X_k         |  [1, n_1 * n_2] array  |  single row of X-ray transform matrix
+# n_1         |  int (even)            |  rows of image
+# n_2         |  int (even)            |  cols of image
+# m           |  int                   |  number of X-rays fired; divides (0,pi)
+# x_grid      |  [n_1+1, 1] array      |  n_1+1 gridlines for n_1 pixels
+# y_grid      |  [n_2+1, 1] array      |  n_2+1 gridlines for n_2 pixels
+# theta       |  [m, 1] array          |  angles dividin (0,pi)
+# i: 1...n_1  |  int                   |  counting index for n_1 (rows of image)
+# j: 1...n_2  |  int                   |  counting index for n_2 (cols of image)
+# k: 1...m    |  int                   |  counting index for m (rays of X-ray)
 
-def siddon_algorithm(X1, Y1, X2, Y2, Xgrid, Ygrid):
+def siddon_algorithm(x1, y1, x2, y2, x_grid, y_grid, debug=False):
     """Uses Siddon's (1985) method to compute cell crossings.
     Source: http://users.elis.ugent.be/~brdsutte/research/publications/1998JCITjacobs.pdf
             https://github.com/aivian/utilities/blob/1390dd0eb5112468bb92c3895b13d4329d0d1553/src/geometry/grid_math.py
     Args:
-        X1: the x coordinate of the first point
-        Y1: the y coordinate of the first point
-        X2: the x coordinate of the end point
-        Y2: the y coordinate of the end point
-        Xgrid: the x coordinates of the gridlines dividing cells
-        Ygrid: the y coordinate of the gridlines dividing cells
+        x1: the x coordinate of the first point
+        y1: the y coordinate of the first point
+        x2: the x coordinate of the end point
+        y2: the y coordinate of the end point
+        x_grid: the x coordinates of the gridlines dividing cells
+        y_grid: the y coordinate of the gridlines dividing cells
     Returns:
-        A list of two numpy arrays
-        cells: the first array is the i coordinate
-            and the second column is the j coordinate
-            of the cell corresponding to l.
-        l: the length of the full line that lies in each cell
-        (cells, l)
+        X_k: k-th 1 x n_1*n_2 array flattened by row
         Note that if a length of the path falls outside of the grid, cells will
         return [-1,-1] in the appropriate row
     """
-    # make Xgrid and Ygrid numpy arrays (harmless if it's already a numpy array)
-    Xgrid = np.array(Xgrid)
-    Ygrid = np.array(Ygrid)
+    # make x_grid and y_grid numpy arrays (harmless if it's already a numpy array)
+    x_grid = np.array(x_grid)
+    y_grid = np.array(y_grid)
     # specify the distance between planes (should be regular here)
-    dx = np.abs(Xgrid[1] - Xgrid[0])
-    dy = np.abs(Ygrid[1] - Ygrid[0])
+    dx = np.abs(x_grid[1] - x_grid[0])
+    dy = np.abs(y_grid[1] - y_grid[0])
     # find the number of grid lines dividing cells, note that there are (Nx-1,Ny-1) voxels in this 2d array
-    Nx = Xgrid.size
-    Ny = Ygrid.size
-
-    print("Nx, Ny: %s, %s" % (Nx,Ny))
+    Nx = x_grid.size
+    Ny = y_grid.size
+    if debug:
+        print("Nx, Ny: %s, %s" % (Nx,Ny))
 
     # calculate the range of parametric values
-    if (X2-X1) != 0.0:
+    if (x2-x1) != 0.0:
         ax = np.zeros(Nx)
-        ax[0] = (Xgrid[0]-X1)/(X2-X1)
-        print("Xgrid[0]: %s" % Xgrid[0])
-        ax[Nx-1] = (Xgrid[Nx-1]-X1)/(X2-X1)
+        ax[0] = (x_grid[0]-x1)/(x2-x1)
+        if debug:
+            print("x_grid[0]: %s" % x_grid[0])
+        ax[Nx-1] = (x_grid[Nx-1]-x1)/(x2-x1)
     else:
         ax = np.zeros(0)
-    if (Y2-Y1) != 0.0:
+    if (y2-y1) != 0.0:
         ay = np.zeros(Ny)
-        ay[0] = (Ygrid[0]-Y1)/(Y2-Y1)
-        print("Ygrid[0]: %s" % Ygrid[0])
-        ay[Ny-1] = (Ygrid[Ny-1]-Y1)/(Y2-Y1)
+        ay[0] = (y_grid[0]-y1)/(y2-y1)
+        if debug:
+            print("y_grid[0]: %s" % y_grid[0])
+        ay[Ny-1] = (y_grid[Ny-1]-y1)/(y2-y1)
     else:
         ay = np.zeros(0)
 
-    print("ax, ay: %s, %s" % (ax,ay))
-    print("len ax, ay: %s, %s" % (len(ax),len(ay)))
+    if debug:
+        print("ax, ay: %s, %s" % (ax,ay))
+        print("len ax, ay: %s, %s" % (len(ax),len(ay)))
 
     if (ax.size > 0) and (ay.size > 0):
         amin = max([0.0, min(ax[0], ax[Nx-1]), min(ay[0], ay[Ny-1])])
@@ -75,38 +80,40 @@ def siddon_algorithm(X1, Y1, X2, Y2, Xgrid, Ygrid):
         amin = 0.0
         amax = 1.0
 
-    print("amin, amax: %s, %s" % (amin,amax))
+    if debug:
+        print("amin, amax: %s, %s" % (amin,amax))
 
     # Calculate the range of indices covered
-    if (X2-X1)>=0:
-        imin = Nx - np.floor((Xgrid[Nx-1] - amin*(X2-X1) - X1)/dx)
-        imax = 1 + np.floor((X1 + amax*(X2-X1) - Xgrid[0])/dx)
+    if (x2-x1)>=0:
+        imin = Nx - np.floor((x_grid[Nx-1] - amin*(x2-x1) - x1)/dx)
+        imax = 1 + np.floor((x1 + amax*(x2-x1) - x_grid[0])/dx)
     else:
-        imin = Nx - np.floor((Xgrid[Nx-1] - amax*(X2-X1) - X1)/dx)
-        imax = 1 + np.floor((X1 + amin*(X2-X1) - Xgrid[0])/dx)
-    if (Y2-Y1)>=0:
-        jmin = Ny - np.floor((Ygrid[Ny-1] - amin*(Y2-Y1) - Y1)/dy)
-        jmax = 1 + np.floor((Y1 + amax*(Y2-Y1) - Ygrid[0])/dy)
+        imin = Nx - np.floor((x_grid[Nx-1] - amax*(x2-x1) - x1)/dx)
+        imax = 1 + np.floor((x1 + amin*(x2-x1) - x_grid[0])/dx)
+    if (y2-y1)>=0:
+        jmin = Ny - np.floor((y_grid[Ny-1] - amin*(y2-y1) - y1)/dy)
+        jmax = 1 + np.floor((y1 + amax*(y2-y1) - y_grid[0])/dy)
     else:
-        jmin = Ny - np.floor((Ygrid[Ny-1] - amax*(Y2-Y1) - Y1)/dy)
-        jmax = 1 + np.floor((Y1 + amin*(Y2-Y1) - Ygrid[0])/dy)
+        jmin = Ny - np.floor((y_grid[Ny-1] - amax*(y2-y1) - y1)/dy)
+        jmax = 1 + np.floor((y1 + amin*(y2-y1) - y_grid[0])/dy)
 
 
-    print("imin, imax: %s, %s" % (imin,imax))
+    if debug:
+        print("imin, imax: %s, %s" % (imin,imax))
 
     # Calculate parametric sets
     if ax.size > 0:
         i = int(imin)
         for p in range(0, (int(imax-imin)+1)):
             # print("imin = %s" % imin)
-            ax[p] = (Xgrid[i-1]-X1)/(X2-X1)
+            ax[p] = (x_grid[i-1]-x1)/(x2-x1)
             i = i + 1
         ax = ax[0:(int(imax-imin)+1)]
     if ay.size > 0:
         j = int(jmin)
         for p in range(0, (int(jmax-jmin)+1)):
             # print("jmin = %s" % jmin)
-            ay[p] = (Ygrid[j-1]-Y1)/(Y2-Y1)
+            ay[p] = (y_grid[j-1]-y1)/(y2-y1)
             j = j + 1
         ay = ay[0:(int(jmax-jmin)+1)]
 
@@ -114,7 +121,7 @@ def siddon_algorithm(X1, Y1, X2, Y2, Xgrid, Ygrid):
     alpha = np.unique(np.hstack([amin, ax, ay, amax]))
 
     # distance from point 1 to point 2
-    d12 = np.sqrt((X2-X1)**2.0+(Y2-Y1)**2.0)
+    d12 = np.sqrt((x2-x1)**2.0+(y2-y1)**2.0)
 
     # calculate voxel lengths
     # The pixel that contains the midpoint of the intersections that bound a
@@ -128,9 +135,9 @@ def siddon_algorithm(X1, Y1, X2, Y2, Xgrid, Ygrid):
         # find the midpoint of each length
         amid = (alpha[m]+alpha[m-1])/2.0;
         # Find the x index
-        i[m] = np.floor((X1 + amid*(X2-X1)-Xgrid[0])/dx) # 0 indexed, otherwise +1
+        i[m] = np.floor((x1 + amid*(x2-x1)-x_grid[0])/dx) # 0 indexed, otherwise +1
         # find the y index
-        j[m] = np.floor((Y1 + amid*(Y2-Y1)-Ygrid[0])/dy) # 0 indexed, otherwise +1
+        j[m] = np.floor((y1 + amid*(y2-y1)-y_grid[0])/dy) # 0 indexed, otherwise +1
     # remove the first index
     l = np.delete(l, 0)
     i = np.delete(i, 0)
@@ -155,41 +162,34 @@ def siddon_algorithm(X1, Y1, X2, Y2, Xgrid, Ygrid):
 
     # put lengths in cell matrix
     inds = zip(i,j)
-    print("inds: %s" % inds)
-    A_k = np.zeros([Nx,Ny])
-    print("A_k shape", A_k.shape)
+    X_k = np.zeros([Nx-1,Ny-1])
+
     for ind_num in range(len(inds)):
         i = inds[ind_num][0]
         j = inds[ind_num][1]
-        A_k[i,j] = l[ind_num]
-    for row in range(Nx):
-        # pprint.pprint(A_k[row,:])
-        print([round(A_k[row,i],2) for i in range(Ny)])
-    # return the cells and the lengths
-    return cells, l
+        X_k[i,j] = l[ind_num]
 
-K = 2
-M = 8
-N = 4
+    # printing
+    if debug:
+        print("inds: %s" % inds)
+        print("X_k shape", X_k.shape)
+        for row in range(Nx-1):
+            # pprint.pprint(X_k[row,:])
+            print([round(X_k[row,i],2) for i in range(Ny-1)])
 
-thetas = np.linspace(0, np.pi, K+1)[0:K]
-ms = np.tan(thetas)
-print("thetas: %s" % thetas)
-print("slopes: %s" % ms)
+    # return X_k
+    X_k = X_k.flatten()
+    return X_k
 
-def gen_grids(M, N):
+def gen_grids(n_1, n_2):
     """
-    generates Xgrid and Ygrid from even M, N
+    generates x_grid and y_grid from even n_1, n_2
     """
-    Xgrid = 1.0*np.arange(-int(N/2), int(N/2)+1)
-    Ygrid = 1.0*np.arange(-int(M/2), int(M/2)+1)
-    return Xgrid, Ygrid
+    x_grid = 1.0*np.arange(-int(n_2/2), int(n_2/2)+1)
+    y_grid = 1.0*np.arange(-int(n_1/2), int(n_1/2)+1)
+    return x_grid, y_grid
 
-Xgrid, Ygrid = gen_grids(M,N)
-print(Xgrid)
-print(Ygrid)
-
-def gen_eep(Xgrid, Ygrid, thetas):
+def gen_eep(x_grid, y_grid, thetas):
     """
     purpose:
     ----------
@@ -198,8 +198,8 @@ def gen_eep(Xgrid, Ygrid, thetas):
 
     params:
     ----------
-    Xgrid: np.array containing discretized x values
-    Ygrid: np.array containing discretized y values
+    x_grid: np.array containing discretized x values
+    y_grid: np.array containing discretized y values
     thetas: list of angles in [0,2pi]
 
     output
@@ -207,51 +207,59 @@ def gen_eep(Xgrid, Ygrid, thetas):
     eepoints: list containing tuples of (x_min, y_min, x_max, y_max)
     """
 
-    x_max = Xgrid[-1]
-    y_max = Ygrid[-1]
+    x_max = x_grid[-1]
+    y_max = y_grid[-1]
     s = y_max/x_max
 
     points = []
     for theta in thetas:
         m = float(np.tan(theta))
         if np.absolute(m) <= s:
-            x_max = float(Xgrid[-1])
-            x_min = float(Xgrid[0])
+            x_max = float(x_grid[-1])
+            x_min = float(x_grid[0])
             y_max = float(m * x_max)
             y_min = float(m * x_min)
             pp = [x_min, y_min, x_max, y_max]
             pp = [round(p,5) for p in pp]
             points.append(pp)
         else:
-            x_max = float(Ygrid[-1]/m)
-            x_min = float(Ygrid[0]/m)
-            y_max = float(Ygrid[-1])
-            y_min = float(Ygrid[0])
+            x_max = float(y_grid[-1]/m)
+            x_min = float(y_grid[0]/m)
+            y_max = float(y_grid[-1])
+            y_min = float(y_grid[0])
             pp = [x_min, y_min, x_max, y_max]
             pp = [round(p,5) for p in pp]
             points.append(pp)
     return points
 
-points = gen_eep(Xgrid=Xgrid, Ygrid=Ygrid, thetas=thetas)
-print(points)
+def gen_X(n_1=None, n_2=None, m=None, sp_rep=False, debug=False):
+    ## handle args
+    if n_1 % 2 == 1:
+        n_1 += 1
+    if n_2 % 2 == 1:
+        n_2 += 1
+    n_1 = int(n_1)
+    n_2 = int(n_2)
+    m = int(m)
 
-x1,y1,x2,y2 = points[1]
-x1,y1,x2,y2 = -2.,-3.,2.,3.
-cells, l = siddon_algorithm(x1,y1,x2,y2,Xgrid,Ygrid)
-print(points[1])
-print(cells)
-print(l)
+    ## generate grid
+    xgrid, ygrid = gen_grids(n_1,n_2)
 
-def gen_X(M=None, N=None, K=None, thetas=None):
-    # TODO: FINISH
-    if M % 2 == 1:
-        M += 1
-    if N % 2 == 1:
-        N += 1
-    M = int(M)
-    N = int(N)
-    K = int(K)
-    thetas = np.linspace(0, np.pi, K+1)[0:K]
-    Xgrid, Ygrid = gen_grids(M,N)
-    for theta in thetas:
-        pass
+    ## partition [0,pi] with m angles
+    theta = np.linspace(0, np.pi, m+1)[0:m]
+
+    ## compute entry/exit points on grid for each slope
+    eepoints = gen_eep(x_grid=xgrid, y_grid=ygrid, thetas=theta)
+
+    ## initialize X-ray transform matrix
+    X = np.zeros([m, n_1*n_2])
+    for k in range(m):
+        x1,y1, x2,y2 = eepoints[k]
+        X[k,:] = siddon_algorithm(x1, y1, x2, y2, xgrid, ygrid, debug=debug)
+    if sp_rep:
+        return(sparse.csr_matrix(X))
+    else:
+        return(X)
+
+X = gen_X(n_1=8, n_2=4, m=3, sp_rep=False, debug=False)
+print(X)
