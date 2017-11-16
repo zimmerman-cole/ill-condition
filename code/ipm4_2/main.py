dfff968f9ad4c1f3fa2961d21f64334f058c045e
jakeroth <jmroth@uchicago.edu>
2017-11-15 23:13:17 -0600

    adding convex problem

---
+++ code/ipm4_2/main.py
@@ -0,0 +1,74 @@
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+import util
+## =============================================================================
+## reference: http://apmonitor.com/me575/index.php/Main/BookChapters
+## =============================================================================
+
+def ipm(x=None, s=None, lam=None, mu=None, K=8, d=5., show=True):
+    ## initialize
+    x, s, lam, mu = util.parse_input(x, s, lam, mu)
+    mu0 = mu
+    xs, ss, lams, ms = [], [], [], []
+    xs_cp = []
+    F = util.F(x, s, lam, mu)
+    if show:
+        xs.append(np.copy(x))
+        ss.append(np.copy(s))
+        lams.append(np.copy(lam))
+        ms.append(la.norm(F,2))
+        xs_cp.append(np.copy(x))
+
+    print("================================================")
+    print("===== at initialization ========================")
+    util.fun_status(x, s, lam, mu)
+    print("===== at initialization ========================")
+    print("================================================\n")
+
+    for k in range(K):
+        print("===== start iter {:d} =============================".format(k))
+        util.status(x, s, lam, mu)
+
+        ## solve
+        F = util.F(x, s, lam, mu)
+        J_F = util.J_F(x, s, lam, mu)
+        p = la.solve(J_F, -F)
+
+        ## update with full step for tracking
+        if show:
+            x_cp = util.get_cp(np.copy(x), np.copy(mu))
+            xs_cp.append(x_cp)
+
+        ## update and step
+        x, s, lam = util.step(x, s, lam, p)
+
+        if show:
+            xs.append(np.copy(x))
+            ss.append(np.copy(s))
+            lams.append(np.copy(lam))
+            ms.append(la.norm(F,2))
+        mu /= d
+        print("===== end iter {:d} ===============================\n".format(k))
+
+    print("================================================")
+    print("===== upon completion ==========================")
+    util.fun_status(x, s, lam, mu)
+    print("------------------------------------------------")
+    if show:
+        print("    x_nt = {:s}".format(xs[-1].T))
+        print("    f(x_nt) = {:s}".format(util.f(xs[-1])))
+        print("    x_cp = {:s}".format(xs_cp[-1]))
+        print("    f(x_cp) = {:0.2f}".format(util.f(xs_cp[-1])))
+    print("===== upon completion ==========================")
+    print("================================================")
+
+    if show:
+        util.visualize(xs, ss, lams, ms, xs_cp, mu0)
+
+if __name__ == '__main__':
+    x = [-3., 12.]
+    s = 2.4375
+    lam = 2
+    mu = 50
+    ipm(x=x, s=s, lam=lam, mu=mu, K=10)
---
+++ code/ipm4_2/util.py
@@ -0,0 +1,284 @@
+import numpy as np
+import numpy.linalg as la
+import scipy
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+np.set_printoptions(precision=2, linewidth=80)
+
+## ===== primary ===============================================================
+def f(x):
+    """
+    obj function
+    """
+    x1, x2 = x[0], x[1]
+    return x1**4 + x2**2 + x1**2 - 2.*x1 + 5.
+
+def fp(x1, x2):
+    """
+    obj function (for plotting)
+    """
+    return x1**4 + x2**2 + x1**2 - 2.*x1 + 5.
+
+def g(x):
+    """
+    constraints (m=1)
+    """
+    x1, x2 = x[0], x[1]
+    return -(x1 + 0.25)**2 + 0.75*x2
+## ===== primary ===============================================================
+
+## ===== derivs ================================================================
+def grad_f(x):
+    """
+    grad of obj function
+    """
+    x1, x2 = x[0], x[1]
+    out = np.zeros([2, 1])
+    out[0] = 4.*x1**3 + 2.*x1 - 2
+    out[1] = 2.*x2
+    return out
+
+def grad_g(x):
+    """
+    grad of constraints (m=1)
+    """
+    x1, x2 = x[0], x[1]
+    m = 1
+    out = np.zeros([2, m])   # note: would be n-x-m for m constraints
+    out[0] = -2.*(x1 + 0.25)
+    out[1] = 0.75
+    return out
+## ===== derivs ================================================================
+
+## ===== second ================================================================
+def hess_f(x):
+    """
+    hess of obj function
+    """
+    x1, x2 = x[0], x[1]
+    out = np.zeros([2, 2])
+    out[0,0] = 12.*x1**2 + 2.
+    out[0,1] = 0.
+    out[1,0] = 0.
+    out[1,1] = 2.
+    return out
+
+def hess_g(x):
+    """
+    hess of constraints (m=1)
+    """
+    x1, x2 = x[0], x[1]
+    out = np.zeros([2, 2])
+    out[0,0] = -2.
+    out[0,1] = 0.
+    out[1,0] = 0.
+    out[1,1] = 0.
+    return out
+
+## ===== other =================================================================
+def dmat(v):
+    """
+    place vector on diagonal
+    """
+    return np.diag(v)
+
+def parse_input(x, s, lam, mu, m=1, n=2):
+    m_param = [s, lam]
+    xx = np.zeros([n, 1])
+    xx[0], xx[1] = x[0], x[1]
+    s, lam = [np.array([float(p)]) for p in m_param]
+    mu = float(mu)
+    return xx, s, lam, mu
+
+def fun_status(x, s, lam, mu):
+    print("----- primary -----------------------------")
+    print("    f = {:s}".format(f(x)))
+    print("    g = {:s}".format(g(x)))
+    print("----- primary -----------------------------\n")
+    print("----- deriv -------------------------------")
+    print("    grad_f = \n{:s}".format(grad_f(x)))
+    print("    grad_g = \n{:s}".format(grad_g(x)))
+    print("----- deriv -------------------------------\n")
+    print("----- rootfinding -------------------------")
+    print("    F = \n{:s}".format(F(x, s, lam, mu)))
+    print("    JF = \n{:s}".format(J_F(x, s, lam, mu)))
+    print("----- rootfinding -------------------------")
+
+def status(x, s, lam, mu):
+    print("----- point -------------------------------")
+    print("    x = {:s}".format(x.T))
+    print("    s = {:s}".format(s))
+    print("    lam = {:s}".format(lam))
+    print("----- point -------------------------------\n")
+    print("----- residual ----------------------------")
+    print("    obj = {:s}".format(F_1(x, lam).T))
+    print("    slack = {:s}".format(F_2(s, lam, mu)))
+    print("    constr = {:s}".format(F_3(x, s)))
+    print("----- residual ----------------------------")
+
+def visualize(xs, ss, lams, ms, xs_cp, mu):
+    ## initialize contours
+    delta = 0.025
+    x1 = np.arange(-4.0, 2.0, delta)
+    x2 = np.arange(-5.0, 15.0, delta)
+    levels = range(4,11)
+    levels += [15, 20, 25, 30, 40, 50, 70, 100, 200, 300]
+    X1, X2 = np.meshgrid(x1,x2)
+    Y = fp(X1, X2)
+
+    fig = plt.figure()
+
+    ## initialize path subplot
+    plt.subplot(211)
+
+    ## contours
+    CS = plt.contour(X1, X2, Y, levels=levels, linewidths=0.5)
+    plt.clabel(CS, inline=1, fontsize=10)
+
+    ## path & constraint & full step
+    plt.plot([xx[0] for xx in xs], [xx[1] for xx in xs], marker=".", label="cp_ntn", linewidth=0.5, markersize=3)
+    plt.plot([xx[0] for xx in xs_cp], [xx[1] for xx in xs_cp], marker=".", label="cp_num", linewidth=0.5, markersize=3)
+    plt.plot(x1, (x1 + 0.25)**2/0.75, label="const = 0", linewidth=0.5, color="red")
+    # for i in range(len(xs)):
+    #     plt.annotate("(s={:s}, lam={:s})".format(ss[i], lams[i]), xy=(xs[i][0], xs[i][1]), textcoords='data')
+    plt.legend(fontsize=5)
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    plt.title("path")
+
+    ## initialize path subplot
+    ax = plt.subplot(212)
+    ax.semilogy(ss, label="slacks", linewidth=0.5)
+    ax.semilogy(ms, label="merits", linewidth=0.5)
+    plt.legend(loc=2, fontsize=5)
+    plt.xlabel("iteration")
+    plt.ylabel("s or merit value")
+    axx = ax.twinx()
+    axx.plot(lams, label="duals", color='red', linewidth=0.5)
+    plt.legend(loc=1, fontsize=5)
+    plt.ylabel("dual")
+    plt.title("s, lambda, and merit")
+
+    plt.tight_layout()
+    plt.show()
+    fig.savefig("output/cp_m{:0.1f}_x1={:0.1f}_x2={:0.1f}.pdf".format(mu, xs[0][0][0], xs[0][1][0]))
+## ===== other =================================================================
+
+## ===== rootfinding ===========================================================
+def F_1(x, lam):
+    """ obj component in gradient of Lagrangian """
+    ## note: not general for m > 1
+    return grad_f(x) - lam*grad_g(x)
+
+def F_2(s, lam, mu):
+    """ slack component in gradient of Lagrangian """
+    e = np.ones([len(s), 1])
+    return dmat(s).dot(dmat(lam)).dot(e) - mu*e
+
+def F_3(x, s):
+    """ constr component in gradient of Lagrangian """
+    return -(g(x) - s)
+
+def F(x, s, lam, mu):
+    """
+    gradient of Lagrangian (full function for rootfinding)
+    """
+    ## initialize
+    assert(len(s) == len(lam))
+
+    ## compute
+    F1 = F_1(x, lam)
+    F2 = F_2(s, lam, mu)
+    F3 = F_3(x, s)
+    out = np.vstack([F1, F2, F3])
+    return out
+
+def J_F(x, s, lam, mu):
+    """
+    Jacobian of Lagrangian (full function for rootfinding)
+    """
+    ## initialize
+    assert(len(s) == len(lam))
+    n, m = x.shape[0], s.shape[0]
+
+    ## compute components
+    JF11 = hess_f(x) - lam*hess_g(x)
+    JF12 = np.zeros([n, m])
+    JF13 = -grad_g(x)
+
+    JF21 = np.zeros([m, n])
+    JF22 = dmat(lam)
+    JF23 = dmat(s)
+
+    JF31 = -grad_g(x).T
+    JF32 = np.eye(m)
+    JF33 = np.zeros([m, m])
+
+    ## combine components
+    out = np.bmat([[JF11, JF12, JF13],
+                   [JF21, JF22, JF23],
+                   [JF31, JF32, JF33]])
+    return out
+## ===== rootfinding ===========================================================
+
+## ===== merit / step ==========================================================
+def update(x, s, lam, p):
+    p = p.reshape(len(p),)
+    px = p[0:2]
+    px = px.reshape(2,1)
+    ps = p[2:3]
+    ps = ps.reshape(1,)
+    plam = p[3:]
+    plam = plam.reshape(1,)
+    x += px
+    s += ps
+    lam += plam
+    return x, s, lam
+
+def step(x, s, lam, p):
+    # a = la.norm(F_1(x, lam), 2)
+    # b = la.norm(F_2(s, lam, mu), 2)
+    # c = la.norm(F_3(x, s), 2)
+    # d = -s
+    # e = -lam
+    alpha = 1
+    i = 0
+    xx, ss, lamlam = update(np.copy(x), np.copy(s), np.copy(lam), np.copy(p))
+    while (ss < 0  or lamlam < 0) and i < 1000:
+        alpha *= 0.9
+        xx, ss, lamlam = update(x, s, lam, alpha*p)
+        i += 1
+    if i > 1000:
+        print("warning")
+    return xx, ss, lamlam
+## ===== merit / step ==========================================================
+
+## ===== scipy optim ===========================================================
+def constraint(x):
+    x1, x2 = x[0], x[1]
+    return -(x1 + 0.25)**2 + 0.75*x2
+
+def objective(x, mu):
+    x1, x2 = x[0], x[1]
+    return x1**4 + x2**2 + x1**2 - 2.*x1 + 5. - mu*np.log(constraint(x))
+
+def get_cp(x, mu):
+    n = 2
+    b = (-10.0, 10.0)
+    bnds = (b, b)
+    con = {'type': 'ineq', 'fun': constraint}
+    cons = ([con])
+    solution = minimize(objective, x, args=mu, method='SLSQP',\
+                        bounds=bnds, constraints=cons)
+    x = solution.x
+
+    # show final objective
+    print('Final SSE Objective: ' + str(objective(x, mu)))
+    # print solution
+    print('Solution')
+    print('x1 = ' + str(x[0]))
+    print('x2 = ' + str(x[1]))
+
+    return x
+
+## ===== scipy optim ===========================================================
